3aedf35e496a7299768d70fb2afea468f6f0ecda
Chris Franklin <cf73021@gmail.com>
2019-01-02 18:01:47 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,14 @@
 class SongsController < ApplicationController
-  
+  def index
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs 
+      end
+    else
+      @songs = Song.all 
+    end
+  end
 end
