c505cea769dbc0dfb4e0bafe332a1a19ead14d91
Chris Franklin <cf73021@gmail.com>
2019-01-02 17:50:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,7 @@ class SongsController < ApplicationController
 
   def edit
     if params[:artist_id]
+      params[:nested] = 1
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
         redirect_to artists_path, alert: "Artist not found."
