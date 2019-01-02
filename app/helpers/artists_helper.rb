4841b6e2fd6523701197b1eee95e9c6dc05ff12e
Chris Franklin <cf73021@gmail.com>
2019-01-02 18:25:27 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,11 +3,13 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select(song,nested)
-    if nested.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+  def artist_select(artist_id, artist)
+    label_tag :artist_id
+    if artist_id
+      hidden_field_tag :artist_id
+      artist.name
     else
-      hidden_field_tag "song[artist_id]", song.artist_id
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
 end
